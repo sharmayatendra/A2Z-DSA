6c587b82a43843ccabd6cbb6ffdb064759237020
sharmayatendra <yatendra948@gmail.com>
2023-09-28 00:51:09 +0530

    feat: Add 1st pattern

--- patterns/pattern1.js
+++ patterns/pattern1.js
@@ -1,1 +1,14 @@
 // we will solve 1st pattern here.
+
+function pattern1(n) {
+    let str = ''
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            str += '*'
+        }
+        str += '\n'
+    }
+    return str
+}
+
+console.log(pattern1(5))
