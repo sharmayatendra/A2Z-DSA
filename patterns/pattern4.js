7123104307fb80196265a66e154f201aa8c67508
sharmayatendra <yatendra948@gmail.com>
2023-10-01 02:29:38 +0530

    feat: Add pattern4
    - Add logic for `pattern4`

---
+++ patterns/pattern4.js
@@ -0,0 +1,21 @@
+/**
+ * Input Format: N = 3
+    Result: 
+        1
+        2 2 
+        3 3 3
+ */
+
+function pattern4(n) {
+    let pattern = 0;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j < i + 1; j++) {
+            pattern = pattern + i + ' ';
+        }
+        pattern += '\n';
+    }
+
+    return pattern
+}
+
+console.log(pattern4(8))
