d7a6c003c40f6dfd29694accb4948aeee6627b40
sharmayatendra <yatendra948@gmail.com>
2024-09-11 21:23:45 +0530

    [pattern]: Add pattern4

--- patterns/newPattern3.js
+++ patterns/newPattern3.js
@@ -10,3 +10,21 @@ function newPattern3(n) {
 }
 
 console.log(newPattern3(2));
+
+// pattern-4
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+function newPattern4(n) {
+  let numberPattern = "";
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i + 1; j++) {
+      numberPattern += i + 1 + " ";
+    }
+    numberPattern += "\n";
+  }
+  return numberPattern;
+}
+
+console.log(newPattern4(3));
